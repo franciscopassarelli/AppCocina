47526a5ebde71f11968555718bb91ad17188c2ad
Francisco <franciscopassarelli7@gmail.com>
2025-07-16 19:38:19 -0300

    deploy

--- src/api/productoService.js
+++ src/api/productoService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/productos";
+const API_URL = import.meta.env.VITE_API_URL + "/api/productos"; // si usás Vite
+
 
 export const obtenerProductos = async () => {
   const res = await axios.get(API_URL);
