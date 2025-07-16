cba9f7ab6c73efbae24e87b219c2e442994d66e6
Francisco <franciscopassarelli7@gmail.com>
2025-07-16 20:21:18 -0300

    redeploy

--- src/api/productoService.js
+++ src/api/productoService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL + "/api/productos"; // si usás Vite
+const API_URL = import.meta.env.VITE_API_URL + "/api/productos";
 
 
 export const obtenerProductos = async () => {
