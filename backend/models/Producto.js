60b0ef18d209fddd37b9a298ee01e460a342c52a
Francisco <franciscopassarelli7@gmail.com>
2025-07-18 13:46:06 -0300

    Sección-departamento

--- backend/models/Producto.js
+++ backend/models/Producto.js
@@ -5,8 +5,8 @@ const productoSchema = new mongoose.Schema({
   stock: { type: Number, required: true },
   unidad: { type: String, enum: ["kg", "l", "unidad"], required: true },
   pesoPromedio: { type: Number, required: true }, // gramos por unidad
-   departamento: { type: String, required: true },
-   stockCritico: { type: Number, default: 0 }, // ⬅️ Este es el nuevo campo
+  departamento: { type: String, required: true },
+  stockCritico: { type: Number, default: 0 }, // ⬅️ Este es el nuevo campo
 });
 
 module.exports = mongoose.model("Producto", productoSchema);
